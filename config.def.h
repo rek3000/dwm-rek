1c5844a7eaa9ee7b9b977c684963fde7880c7d5b
rek3000 <torek3k@gmail.com>
2023-10-06 11:22:08 +0700

    added patches

--- config.def.h
+++ config.def.h
@@ -9,8 +9,10 @@ static const int showbar            = 1;        /* 0 means no standard bar */
 static const int topbar             = 1;        /* 0 means standard bar at bottom */
 static const int extrabar           = 1;        /* 0 means no extra bar */
 static const char statussep         = ';';      /* separator between statuses */
-static const char *fonts[]          = { "Rursus Compact Mono:size=12" };
-static const char dmenufont[]       = "Rursus Compact Mono:size=12";
+//static const char *fonts[]          = { "Rursus Compact Mono:size=12" };
+//static const char dmenufont[]       = "Rursus Compact Mono:size=12";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
